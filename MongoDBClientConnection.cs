d45c91ed2917672c450c5b6b751ba6db4dd97549
Prashansa Meyn <pm@globalpay.dk>
2022-07-17 21:55:18 +0200

    intial commit

---
+++ MongoDBClientConnection.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UmbracoMongoDbClient
+{
+	public static class MongoDBClientConnection
+	{
+		private static MongoClient MongoClient;
+		private static string _databaseNamePrefix;
+
+		public static void Setup(X509Certificate2 cert, string connectionString, string databaseNamePrefix)
+		{
+			var settings = MongoClientSettings.FromConnectionString(connectionString);
+
+			settings.SslSettings = new SslSettings
+			{
+				ClientCertificates = new List<X509Certificate>() { cert }
+			};
+
+			MongoClient = new MongoClient(settings);
+			_databaseNamePrefix = databaseNamePrefix;
+		}
+
+		public static void Setup(string connectionStringWithPassword, string databaseNamePrefix)
+		{
+			var settings = MongoClientSettings.FromConnectionString(connectionStringWithPassword);
+			settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+			MongoClient = new MongoClient(settings);
+			_databaseNamePrefix = databaseNamePrefix;
+		}
+
+		public static IMongoDatabase GetDatabase(string dBName) => MongoClient.GetDatabase($"{_databaseNamePrefix}-{dBName}");
+	}
+}
