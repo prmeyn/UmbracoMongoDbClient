62274380a6fd92d4d1f6b380c769bc9064fa9baf
Prashansa Meyn <pm@globalpay.dk>
2022-09-18 14:05:51 +0200

    time watch

--- Setup/MongoDbComponent.cs
+++ Setup/MongoDbComponent.cs
@@ -2,6 +2,8 @@
 using Umbraco.Cms.Core.Composing;
 using Microsoft.AspNetCore.Hosting;
 using Umbraco.Extensions;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace UmbracoMongoDbClient.Setup
 {
@@ -9,18 +11,31 @@ namespace UmbracoMongoDbClient.Setup
 	{
 		private readonly IHostingEnvironment _env;
 		private readonly IConfiguration _config;
+		private readonly ILogger<MongoDbComponent> _logger;
 
-		public MongoDbComponent(IHostingEnvironment env, IConfiguration config)
+		public MongoDbComponent(IHostingEnvironment env, IConfiguration config, ILogger<MongoDbComponent> logger)
 		{
 			_env = env;
 			_config = config;
+			_logger = logger;
 		}
 
 		public void Initialize()
 		{
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
 			MongoDBClientConnection.Initialize(
 				connectionStringWithPassword: _config.GetUmbracoConnectionString("mongoDBConnectionStringWithPassword"),
 				databaseNamePrefix: _env.EnvironmentName);
+			stopWatch.Stop();
+			// Get the elapsed time as a TimeSpan value.
+			TimeSpan ts = stopWatch.Elapsed;
+
+			// Format and display the TimeSpan value.
+			string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+				ts.Hours, ts.Minutes, ts.Seconds,
+				ts.Milliseconds / 10);
+			_logger.LogInformation("Time taken to establish MongoDB initial connection {elapsedTime}", elapsedTime);
 		}
 
 		public void Terminate()
